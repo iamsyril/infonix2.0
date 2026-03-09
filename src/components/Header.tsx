a6544e23fbcc0d9b9329f584d00a27bcc8ae4452
syril <syrilarockiaraj21@gmail.com>
2026-03-09 21:29:59 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,11 +17,7 @@ const Header = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center space-x-3">
-            <img 
-              src={infonixLogo} 
-              alt="Infonix Logo" 
-              className="h-10 w-auto object-contain"
-            />
+            
             <span className="font-heading font-bold text-xl text-foreground">
               
             </span>
